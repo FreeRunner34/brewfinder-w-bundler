af4f549d07efe71dae665655bc704b5234264198
FreeRunner34 <64115389+FreeRunner34@users.noreply.github.com>
2020-12-08 12:49:36 -0800

    Create environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require "pry"
+require 'rubygems'
+require 'httparty'
+
+# require my classes/modules
+require_relative "../lib/cli"
+require_relative "../lib/Brewfinder"
+require_relative "../lib/api"
