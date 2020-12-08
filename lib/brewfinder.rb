f6a64fac5f2e934f41786f0de01799fce1eaa146
FreeRunner34 <64115389+FreeRunner34@users.noreply.github.com>
2020-12-08 12:45:47 -0800

    Update brewfinder.rb

--- lib/brewfinder.rb
+++ lib/brewfinder.rb
@@ -4,3 +4,53 @@ module Brewfinder
   class Error < StandardError; end
   # Your code goes here...
 end
+
+class Brewery
+
+  attr_accessor :id, :name, :brewery_type, :street, :address_2, :address_3, :city, :state, :county_province, :postal_code, :country, :longitude, :latitude, :phone, :website_url, :updated_at, :created_at
+
+  @@all = []
+
+  def initialize(attributes)
+      attributes.each do |key, value| 
+          self.class.attr_accessor(key)
+          self.send(("#{key}="), value)
+          end
+          @@all << self 
+  end 
+
+  def self.all 
+      @@all 
+  end
+
+  def save 
+      @@all << self 
+  end 
+
+  def self.reset_all
+      @@all.clear
+  end 
+
+  def self.count 
+      @@all.size
+  end 
+  #binding.pry
+end 
+
+# id
+# name
+# brewery_type
+# street
+# address_2
+# address_3
+# city
+# state
+# county_province
+# postal_code
+# country
+# longitude
+# latitude
+# phone
+# website_url
+# updated_at
+# created_at
